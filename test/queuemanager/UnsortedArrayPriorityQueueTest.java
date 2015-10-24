39bdaa7d42eeb8ac34bae7dc3192f5a441ec2af1
Jamie Simpson <Jamie Simpson@JamieSimpson-PC.home>
2015-10-24 19:57:54 +0100

    Added:- -addHelper() method for populating an array in test class -code for testAdd() method -code for add() method exception handling

--- test/queuemanager/UnsortedArrayPriorityQueueTest.java
+++ test/queuemanager/UnsortedArrayPriorityQueueTest.java
@@ -58,10 +58,72 @@ public class UnsortedArrayPriorityQueueTest {
     }
 
     /**
-     * Test of add method, of class UnsortedArrayPriorityQueue.
+     * Test of add method, of class UnsortedArrayPriorityQueue. This test
+     * (although looking complicated) Creates people, adds them and loops
+     * through the storage of the UnsortedArrayPriorityQueue class taking out
+     * the items and comparing them to the person objects we create and
+     * initialise. We always know the priorities and so we can compare them
+     * manually
+     *
+     * The method also tests that the order is the same order as they go in.
+     *
+     * @throws java.lang.Exception
      */
     @Test
     public void testAdd() throws Exception {
+        unsorted = new UnsortedArrayPriorityQueue(1);
+        //Create person1
+        Person person1 = new Person("Person 1");
+        //Add person1 with priority of 1
+        unsorted.add(person1, 1);
+        //Cast the first value in the array (we know this is 0) to a Wrapper of type Person.
+        Wrapper<Person> person = (Wrapper<Person>) unsorted.getStorage()[0];
+        //This wrapper item should equal the person1 object that we added
+        assertEquals(person.getItem(), person1);
+        //The persons priority is 1 and we know that already
+        assertEquals(person.getPriority(), 1);
+        //Call the add helper method to setup and populate new UnsortedArrayPriorityQueue
+        addHelper();
+        //Loop through the stored array getting the person at each position check if matches
+        for (int i = 0; i < unsorted.getStorage().length; i++) {
+            person = (Wrapper<Person>) unsorted.getStorage()[i];
+            String item = person.getItem().toString();
+            System.out.println(item);
+            assertEquals("Person " + ++i, item);
+            --i;
+        }
+        //**check the priorities were not changed**\\
+        //First person has priority 2
+        person = (Wrapper<Person>) unsorted.getStorage()[0];
+        assertEquals(2, person.getPriority());
+        //Second person has priority 3
+        person = (Wrapper<Person>) unsorted.getStorage()[1];
+        assertEquals(3, person.getPriority());
+        //Third person has priority 1
+        person = (Wrapper<Person>) unsorted.getStorage()[2];
+        assertEquals(1, person.getPriority());
+        //Fourth person has priority 5
+        person = (Wrapper<Person>) unsorted.getStorage()[3];
+        assertEquals(5, person.getPriority());
+        //Fifth person has priority 4
+        person = (Wrapper<Person>) unsorted.getStorage()[4];
+        assertEquals(4, person.getPriority());
+    }
+
+    /**
+     * This tests the add methods ability to not add a value when the queue is
+     * full. a QueueOverflowException is expected when running the test and thus
+     * no asserts are required
+     *
+     * @throws QueueOverflowException
+     */
+    @Test(expected = QueueOverflowException.class)
+    public void testAddBeyondCapacity() throws QueueOverflowException {
+        //**Check that items cannot be added beyond capacity
+        //populate array again
+        addHelper();
+        //try adding new person where limit is 5
+        unsorted.add(new Person("Exception person"), 99);
     }
 
     /**
@@ -141,6 +203,18 @@ public class UnsortedArrayPriorityQueueTest {
     @Test
     public void testToString() throws QueueOverflowException {
 
+    }
+
+    public void addHelper() throws QueueOverflowException {
+        //new instance with space for 5 people
+        unsorted = new UnsortedArrayPriorityQueue(5);
+        //Add 5 people to the array
+        unsorted.add(new Person("Person 1"), 2);
+        unsorted.add(new Person("Person 2"), 3);
+        unsorted.add(new Person("Person 3"), 1);
+        unsorted.add(new Person("Person 4"), 5);
+        unsorted.add(new Person("Person 5"), 4);
+        System.out.println("Below is a list of stored people names");
     }
 
 }
