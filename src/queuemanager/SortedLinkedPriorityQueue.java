2345f0273b9e5afff6c2e9b85e5cce9215b7b540
Jamie Simpson <Jamie Simpson@JamieSimpson-PC.home>
2015-10-26 14:36:06 +0000

    Added:- -SortedLinkedPriorityQueue class - methods for the class
    Note: Methods yet to be implemented

---
+++ src/queuemanager/SortedLinkedPriorityQueue.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Jamie Simpson
+ */
+public class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+
+    private LinkedList<T> storage = new LinkedList<>();
+
+    public SortedLinkedPriorityQueue() {
+    }
+
+    @Override
+    public T head() {
+        //To be implemented
+        return null;
+    }
+
+    @Override
+    public void add(T item, int priority) {
+        //To be implemented
+
+    }
+
+    @Override
+    public void remove() {
+        //To be implemented
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //To be implemented
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        //To be implemented
+        return null;
+    }
+
+}
